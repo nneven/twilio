23c570ac21bcffcdd660401e9694a619ab645ad8
Bebop <nneven@usc.edu>
2023-11-13 15:10:50 -0500

    Update puppeteer.js

--- quickstart-node/src/utils/puppeteer.js
+++ quickstart-node/src/utils/puppeteer.js
@@ -5,6 +5,7 @@ export async function scheduleEvent(url, fullName, email) {
   const page = await browser.newPage();
 
   await page.goto(url);
+  await page.setViewport({ width: 1920, height: 1080 });
 
   await page.waitForSelector("#full_name_input");
   await page.type("#full_name_input", fullName);
@@ -12,15 +13,17 @@ export async function scheduleEvent(url, fullName, email) {
   await page.waitForSelector("#email_input");
   await page.type("#email_input", email);
 
-  await page.waitForSelector(
-    "button.b1hdxvdx.u1xbh6v5.b1qhbqhv.d1bkmipt.smqpqx8.f41u73q"
-  );
-  await page.click(
-    "button.b1hdxvdx.u1xbh6v5.b1qhbqhv.d1bkmipt.smqpqx8.f41u73q"
-  );
+  // await page.waitForSelector(
+  //   "button.b1hdxvdx.u1xbh6v5.b1qhbqhv.d1bkmipt.smqpqx8.f41u73q"
+  // );
+  // await page.click(
+  //   "button.b1hdxvdx.u1xbh6v5.b1qhbqhv.d1bkmipt.smqpqx8.f41u73q"
+  // );
+
+  await page.waitForSelector('button[type="submit"]');
+  await page.click('button[type="submit"]');
 
   await page.waitForNavigation();
-
   await browser.close();
 
   return true;
