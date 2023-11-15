939ffbef5c8326a06ed55f605b6babdf196eb515
Bebop <nneven@usc.edu>
2023-11-15 10:16:00 -0500

    Update translate.py

--- quickstart-node/src/utils/translate.py
+++ quickstart-node/src/utils/translate.py
@@ -1,23 +1,25 @@
 import os
-import whisper
 import warnings
-import playsound
 import subprocess
+
+import whisper
+import playsound
 import soundfile as sf
 import sounddevice as sd
 
 warnings.filterwarnings("ignore", category=UserWarning)
 
 # Load Whisper model
+print("Loading Whisper model...")
 model = whisper.load_model("medium")
 
 # Record audio for 5 seconds
-fs=24000
+FS=24000
 print("Recording...")
-recording = sd.rec(int(5 * fs), samplerate=fs, channels=1, dtype="float32")
+recording = sd.rec(int(5 * FS), samplerate=FS, channels=1, dtype="float32")
 sd.wait()  # Wait until recording is finished
 print("Recording finished. Saving to file...")
-sf.write("recording.wav", recording, fs)
+sf.write("recording.wav", recording, FS)
 
 # Transcribe the audio file
 print("Translating audio...")
@@ -29,11 +31,10 @@ print(transcribed_text)
 command = ["node", "src/utils/playht.js", transcribed_text]
 
 # Run the command and print the output
-process = subprocess.run(command, capture_output=True, text=True)
+process = subprocess.run(command, capture_output=True, text=True, check=True)
 print(process.stdout)
 
 # Play the audio file
-print("Playing translation...")
 playsound.playsound("translation.mp3")
 
 # Delete audio files
